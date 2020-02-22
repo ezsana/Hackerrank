670924251a53706dc24b37ad269f16e8d4d3443c
Zsuzsanna Erdos <ezsana@gmail.com>
2020-02-22 22:14:46 +0100

    BigSorting & tests

--- src/main/java/com/codecool/zsana/hackerrank/BigSorting.java
+++ src/main/java/com/codecool/zsana/hackerrank/BigSorting.java
@@ -1,8 +1,48 @@
 package com.codecool.zsana.hackerrank;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 // Easy 20 points
 public class BigSorting {
 
     String url = "https://www.hackerrank.com/challenges/big-sorting/problem";
 
+    static String[] bigSorting(String[] unsorted) {
+        for (String no : unsorted) {
+            try {
+                BigInteger n = new BigInteger(no);
+                if (n.compareTo(BigInteger.valueOf(0)) < 1) {
+                    return null;
+                }
+            } catch (NumberFormatException nf) {
+                return null;
+            }
+        }
+        List<BigInteger> list = stringToBigIntList(unsorted);
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        return intToStringArray(list);
+    }
+
+    static List<BigInteger> stringToBigIntList(String[] s) {
+        List<BigInteger> numbers = new ArrayList<BigInteger>();
+        for (int i = 0; i < s.length; i++) {
+            BigInteger b = new BigInteger(s[i]);
+            numbers.add(b);
+        }
+        return numbers;
+    }
+
+    static String[] intToStringArray(List<BigInteger> l) {
+        String[] s = new String[l.size()];
+        for (int i = 0; i < l.size(); i++) {
+            s[i] = String.valueOf(l.get(i));
+        }
+        return s;
+    }
 }
---
+++ src/test/java/com/codecool/zsana/hackerrank/BigSortingTest.java
@@ -0,0 +1,82 @@
+package com.codecool.zsana.hackerrank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BigSortingTest {
+
+ /*
+     Acceptance criteria is not clear:
+      - Output Format: Print each element of the sorted array on a new line.
+        OR!!!
+      - Complete the bigSorting function in the editor. It should return the sorted string array.
+      => Should it be the return value AND printed out as well???
+      => Should be the returned String[] looking like this: {"1\n", "2\n"...etc} ???
+      Conclusion:
+      Manual test + automated tests: print is checked manually, return String[] checked with aut tests.
+ */
+
+    @Test
+    public void testWithNegativeNumbers() {
+        String[] s = {"-2", "1", "1"};
+        Assertions.assertNull(BigSorting.bigSorting(s));
+    }
+
+    @Test
+    public void testWithZero() {
+        String[] s = {"0", "2"};
+        Assertions.assertNull(BigSorting.bigSorting(s));
+    }
+
+    @Test
+    public void testWithNonIntegerNumber() {
+        String[] s = {"2.4", "3"};
+        Assertions.assertNull(BigSorting.bigSorting(s));
+    }
+
+    @Test
+    public void testWithNonNumbers() {
+        String[] s = {"3", "ff"};
+        Assertions.assertNull(BigSorting.bigSorting(s));
+    }
+
+    @Test
+    public void testWithEmptyArray() {
+        String[] s = {};
+        Assertions.assertArrayEquals(s, BigSorting.bigSorting(s));
+    }
+
+    @Test
+    public void testWithOneNumberInArray() {
+        String[] s = {"6"};
+        Assertions.assertArrayEquals(s, BigSorting.bigSorting(s));
+        /*  Manual:
+         *      - check if printed out: 6
+         */
+    }
+
+    @Test // Happy path
+    public void testWithPositiveIntegers() {
+        String[] s = {"1000", "342", "12", "1", "8", "367812", "10000001"};
+        String[] sortedS = {"1", "8", "12", "342", "1000", "367812", "10000001"};
+        Assertions.assertArrayEquals(sortedS, BigSorting.bigSorting(s));
+        /*  Manual:
+         *      - check if printed out:
+         *          1
+         *          8
+         *          12
+         *          342
+         *          1000
+         *          367812
+         *          10000001
+         */
+    }
+
+    @Test
+    public void hackerTest1() {
+        String[] s = {"8", "1", "2", "100", "12303479849857341718340192371", "3084193741082937", "3084193741082938", "111", "200"};
+        String[] sortedS = {"1", "2", "8", "100", "111", "200", "3084193741082937", "3084193741082938", "12303479849857341718340192371"};
+        Assertions.assertArrayEquals(sortedS, BigSorting.bigSorting(s));
+    }
+
+}
