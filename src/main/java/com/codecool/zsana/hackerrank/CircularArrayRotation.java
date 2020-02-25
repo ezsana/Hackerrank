31e7359a2d63ce97ca14c9850588ba94bb1ba100
Zsuzsanna Erdos <ezsana@gmail.com>
2020-02-25 18:22:15 +0100

    Circular Array Rotation @ Tests

--- src/main/java/com/codecool/zsana/hackerrank/CircularArrayRotation.java
+++ src/main/java/com/codecool/zsana/hackerrank/CircularArrayRotation.java
@@ -5,4 +5,25 @@ public class CircularArrayRotation {
 
     String url = "https://www.hackerrank.com/challenges/circular-array-rotation/problem";
 
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        if (a.length == 0) {
+            return null;
+        } else if (a.length == 1) {
+            return a;
+        }
+        for (int i = 1; i <= k; i++) {
+            int[] newArr = new int[a.length];
+            for (int y = 1; y < a.length; y++) {
+                newArr[0] = a[a.length-1];
+                newArr[y] = a[y-1];
+            }
+            a = newArr;
+        }
+        int[] indicesValues = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            indicesValues[i] = a[queries[i]];
+        }
+        return indicesValues;
+    }
+
 }
