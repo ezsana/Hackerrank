e710004cbd2abfe77bf2de166b7bc78b8be34410
Zsuzsanna Erdos <ezsana@gmail.com>
2020-02-27 21:00:15 +0100

    Find digits @ Tests

--- src/main/java/com/codecool/zsana/hackerrank/FindDigits.java
+++ src/main/java/com/codecool/zsana/hackerrank/FindDigits.java
@@ -1,8 +1,31 @@
 package com.codecool.zsana.hackerrank;
 
+import java.util.Arrays;
+
 // Easy 25 points
 public class FindDigits {
 
     String url = "https://www.hackerrank.com/challenges/find-digits/problem";
 
+    static int findDigits(int n) {
+        int[] digits = getDigits(n);
+        int countDividers = 0;
+        for (int i : digits) {
+            try {
+                if (n % i == 0) {
+                    countDividers++;
+                }
+            } catch (ArithmeticException ar) {}
+        }
+        return countDividers;
+    }
+
+    public static int[] getDigits(int number) {
+        String num = String.valueOf(number);
+        int[] numbers = new int[num.length()];
+        for (int i = 0; i < num.length(); i++) {
+            numbers[i] = Integer.valueOf(String.valueOf(num.charAt(i)));
+        }
+        return numbers;
+    }
 }
---
+++ src/test/java/com/codecool/zsana/hackerrank/FindDigitsTest.java
@@ -0,0 +1,33 @@
+package com.codecool.zsana.hackerrank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FindDigitsTest {
+
+    @Test
+    public void zeroAsParameter() {
+        Assertions.assertEquals(0, FindDigits.findDigits(0));
+    }
+
+    @Test
+    public void zeroInNumber() {
+        Assertions.assertEquals(1, FindDigits.findDigits(1000));
+        Assertions.assertEquals(0, FindDigits.findDigits(2003));
+        Assertions.assertEquals(2, FindDigits.findDigits(2500));
+    }
+
+    @Test
+    public void moreDigitsAreTheSame() {
+        Assertions.assertEquals(2, FindDigits.findDigits(22));
+        Assertions.assertEquals(3, FindDigits.findDigits(4335));
+    }
+
+    @Test
+    public void noSameDigits() {
+        Assertions.assertEquals(2, FindDigits.findDigits(1234));
+        Assertions.assertEquals(1, FindDigits.findDigits(136589));
+        Assertions.assertEquals(3, FindDigits.findDigits(324516987));
+    }
+
+}
