8cee4aa299ffaf30ad8d2828924eddc36ddf8ab3
Zsuzsanna Erdos <ezsana@gmail.com>
2020-03-01 19:34:56 +0100

    Flatland Space Station & tests.

---
+++ src/test/java/com/codecool/zsana/hackerrank/FlatlandSpaceStationsTest.java
@@ -0,0 +1,52 @@
+package com.codecool.zsana.hackerrank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FlatlandSpaceStationsTest {
+
+    @Test
+    void emptyArray() {
+        int[] arr = {};
+        Assertions.assertEquals(0, FlatlandSpaceStations.flatlandSpaceStations(0, arr));
+    }
+
+    @Test
+    void oneElementArray() {
+        int[] arr = {0};
+        Assertions.assertEquals(0, FlatlandSpaceStations.flatlandSpaceStations(1, arr));
+    }
+
+    @Test
+    void onlyFirstCityHasStation() {
+        int[] arr = {0};
+        Assertions.assertEquals(5, FlatlandSpaceStations.flatlandSpaceStations(6, arr));
+    }
+
+    @Test
+    void onlyLastCityHasStation() {
+        int[] arr = {5};
+        Assertions.assertEquals(5, FlatlandSpaceStations.flatlandSpaceStations(6, arr));
+    }
+
+    @Test
+    void allCitiesHaveStations() {
+        int[] arr = {0,1,2,3,4,5};
+        Assertions.assertEquals(0, FlatlandSpaceStations.flatlandSpaceStations(6, arr));
+    }
+
+    @Test
+    void someCitiesHaveStations() {
+        int[] arr = {0,4,9};
+        int[] arr1 = {4,10};
+        Assertions.assertEquals(2, FlatlandSpaceStations.flatlandSpaceStations(10, arr));
+        Assertions.assertEquals(4, FlatlandSpaceStations.flatlandSpaceStations(11, arr1));
+    }
+
+    @Test
+    void hackerrankTest() {
+        int[] arr = {13,1,11,10,6};
+        Assertions.assertEquals(6, FlatlandSpaceStations.flatlandSpaceStations(20, arr));
+    }
+
+}
