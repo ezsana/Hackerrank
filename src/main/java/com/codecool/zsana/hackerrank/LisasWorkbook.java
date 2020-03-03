ce66b0dcf102f5c7bcdf560b2243f7fcfa2a4f68
Zsuzsanna Erdos <ezsana@gmail.com>
2020-03-03 11:11:15 +0100

    Lisa's workbook & tests.

--- src/main/java/com/codecool/zsana/hackerrank/LisasWorkbook.java
+++ src/main/java/com/codecool/zsana/hackerrank/LisasWorkbook.java
@@ -1,8 +1,25 @@
 package com.codecool.zsana.hackerrank;
 
 // Easy 25 points
-public class LisasWorkbook {
+class LisasWorkbook {
 
     String url = "https://www.hackerrank.com/challenges/lisa-workbook/problem";
 
+    static int workbook(int numberOfChapters, int maxProblemsOnPage, int[] problemsInChapter) {
+        int page = 1;
+        int counter = 0;
+        for (int c : problemsInChapter) {
+            for (int i = 0; i < c; i++) {
+                if (i+1 == page) {
+                    counter++;
+                }
+                if ((i+1) % maxProblemsOnPage == 0) {
+                    page++;
+                } else if (i == c-1) {
+                    page++;
+                }
+            }
+        }
+        return counter;
+    }
 }
