60a79d110e829d84b65a3efbca30f259c5eb37d4
Zsuzsanna Erdos <ezsana@gmail.com>
2020-03-05 12:15:02 +0100

    Strange Counter & Tests

--- src/main/java/com/codecool/zsana/hackerrank/StrangeCounter.java
+++ src/main/java/com/codecool/zsana/hackerrank/StrangeCounter.java
@@ -1,8 +1,23 @@
 package com.codecool.zsana.hackerrank;
 
 // Easy 30 points
-public class StrangeCounter {
+class StrangeCounter {
 
     String url = "https://www.hackerrank.com/challenges/strange-code/problem";
+
+    static long strangeCounter(long t) {
+        long timer = 1;
+        long counter = 3;
+        while (timer <= t) {
+            for (long i = counter; i >= 1; i--) {
+                if (timer == t) {
+                    return i;
+                }
+                timer++;
+            }
+            counter = counter*2;
+        }
+        return -1;
+    }
 
 }
---
+++ src/test/java/com/codecool/zsana/hackerrank/StrangeCounterTest.java
@@ -0,0 +1,43 @@
+package com.codecool.zsana.hackerrank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StrangeCounterTest {
+
+    @Test
+    void test1() {
+        Assertions.assertEquals(1, StrangeCounter.strangeCounter(3));
+    }
+
+    @Test
+    void test2() {
+        Assertions.assertEquals(5, StrangeCounter.strangeCounter(5));
+    }
+
+    @Test
+    void test3() {
+        Assertions.assertEquals(3, StrangeCounter.strangeCounter(7));
+    }
+
+    @Test
+    void test4() {
+        Assertions.assertEquals(7, StrangeCounter.strangeCounter(15));
+    }
+
+    @Test
+    void test5() {
+        Assertions.assertEquals(9, StrangeCounter.strangeCounter(13));
+    }
+
+    @Test
+    void test6() {
+        Assertions.assertEquals(24, StrangeCounter.strangeCounter(22));
+    }
+
+    @Test
+    void test7() {
+        Assertions.assertEquals(18, StrangeCounter.strangeCounter(28));
+    }
+
+}
